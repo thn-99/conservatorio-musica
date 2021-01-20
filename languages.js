f4fbcfd644f4c17d649d33e40f0e07c0af97a824
Tahseen Ahmad <thn.ahmd99@gmail.com>
2021-01-20 16:29:02 +0100

    idiomas no se habia añadido

---
+++ languages.js
@@ -0,0 +1,50 @@
+let en = {};
+if(!localStorage.getItem('language')){
+    localStorage.setItem('language','es');
+}
+let language = localStorage.getItem('language');
+let textSignIn;
+let textHome;
+let textFgtPass;
+let textNotAMember;
+let textRegister;
+let textMail;
+let textPassw;
+
+
+switch(language){
+    case 'en':
+        textSignIn='Sign in';
+        textHome='Home';
+        textFgtPass='Forgon password?';
+        textNotAMember='Not a member?';
+        textRegister='Register';
+        textMail='Email';
+        textPassw='Password';
+    break;
+
+    case 'es':
+        textSignIn='Iniciar Sesión';
+        textHome='Casa';
+        textFgtPass='Contaseña olvidada?';
+        textNotAMember='No estas registrado?';
+        textRegister='Registrate';
+        textMail='Email';
+        textPassw='Contaseña';
+    break;
+}
+
+
+function setLanguageEs(){
+    if(localStorage.getItem('language')!='es'){
+        localStorage.setItem('language','es');
+        location.reload();
+    }
+}
+
+function setLanguageEn(){
+    if(localStorage.getItem('language')!='en'){
+        localStorage.setItem('language','en');
+        location.reload();
+    }
+}
---
+++ master.js
@@ -0,0 +1,110 @@
+let usuario;
+function navbar(){
+    let body = document.getElementById("navegacion")
+    let nav = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            ${!sesionUsuario()?
+            '<li class="nav-item"> Iniciar Sesion </li>':
+            '<li class="nav-item"> <a class="nav-link disabled" href="#">Usuario'+usuario+'</a> </li> <li class="nav-item"> Cerrar Sesión </li>'}
+          
+        </li>
+      </ul>
+    </div>
+    <form class="form-inline my-2 my-lg-0">
+        <button class="btn ${language=='es'?'btn-secondary':'btn-primary'}  my-2 my-sm-0" type="submit" ${language=='es'?'':'disabled'} onclick="setLanguageEn()">English</button>
+        <button class="btn ${language=='es'?'btn-primary':'btn-secondary'}  my-2 my-sm-0" type="submit" ${language=='es'?'disabled':''} onclick="setLanguageEs()">Spanish</button>
+      </form>
+  </nav>
+  `;
+  body.innerHTML+=nav;
+}
+
+function loginForm(){
+    let nose = `    <h1>Conservatorio Música</h1>
+
+    <div id="login">
+        Correo: <input type="text" id="usuario">
+        Contraseña: <input type="text" id="clave">
+        <input type="button" value="Identificarse" onclick="login()">
+    </div>0`;
+
+
+    let loginForm=`
+            <div class="row mb-4">
+            <div class="col-sm-12 d-flex justify-content-center">
+                <h2>${textSignIn}</h2>
+            </div>
+        </div>
+
+        <div>
+            <div class="form-row justify-content-center mb-4">
+                <div class="col-sm-4">
+                    <input type="email" class="form-control" placeholder="${textMail}"  id="correo">
+
+                </div>
+            </div>
+            <div class="form-row justify-content-center mb-4">
+                <div class="col-sm-4">
+                    <input type="password" class="form-control" placeholder="${textPassw}" id="clave">
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col-sm-12 d-flex justify-content-center">
+                    <a href="#">${textFgtPass}</a>
+                </div>
+            </div>
+
+            <div class="row mb-2 justify-content-center">
+                    <button class="col-sm-2 btn btn-info" onclick="login()">${textSignIn}</button>
+                
+            </div>
+            <div class="row mb-2 ">
+                <div class="col-sm-12 d-flex justify-content-center">
+                    <span>${textNotAMember} </span><a href="#">${textRegister}</a>
+                </div>
+            </div>
+
+        </div>
+    `;
+
+    document.getElementById("app").innerHTML+=loginForm;
+
+    
+}
+function login(){
+    let correo = document.getElementById("correo").value;
+    let clave = document.getElementById("clave").value;
+    let respuesta = serverController('login', [["correo", correo], ["clave", clave]]);
+    console.log(respuesta);
+        if (respuesta.estado!="true") {
+            alert(respuesta.mensaje);
+            return false;
+        } else {
+            alert(correcto);
+            return false;
+        }
+}
+
+function sesionUsuario(){
+    let respuesta = serverController('compruebaSesion');
+    if(respuesta.estado=="true"){
+        usuario = respuesta.mensaje;
+        return true;
+    }else{
+        return false;
+    }
+}
